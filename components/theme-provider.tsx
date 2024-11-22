280c3badad77a158bccc6a8cf81595bf7ad724d5
Guillermo <guisalmo@gmail.com>
2024-11-23 00:21:59 +0100

    eslint cleaning

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,17 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  storageKey?: string
+  forcedTheme?: string
+  disableTransitionOnChange?: boolean
+  themes?: string[]
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
